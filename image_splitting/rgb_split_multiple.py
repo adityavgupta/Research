2cce2668dd234f8f34b1529c7031186df5db0068
Aditya <gupta.adityav@gmail.com>
2019-07-24 16:35:41 -0500

    commented and updated some lines

--- image_splitting/rgb_split_multiple.py
+++ image_splitting/rgb_split_multiple.py
@@ -5,13 +5,18 @@ import cv2
 import os
 import argparse
 
+#construct argument parser
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", type=str, default="SVD_study", help="path to load the directory containing the images to be split")
 args = vars(ap.parse_args())
 
+#make a list of directories
 imagePaths = paths.list_images(args["dataset"])
 
-k=469
+#set counter to name output files
+k=1
+
+#for each image inside each directory split the image into its rgb channel
 for imagePath in imagePaths:
 	image = Image.open(imagePath)
 	data = image.getdata() 
@@ -21,15 +26,20 @@ for imagePath in imagePaths:
 	g = [(0, d[1], 0) for d in data]
 	b = [(0, 0, d[2]) for d in data]
 
-	#image.putdata(r)
-	#image.save('r%s.png' % k)
-	
+    #save each image with status on the number of image reached
+	image.putdata(r)
+	image.save('r%s.png' % k)
+	print('r%s.png' % k)
+    
 	image.putdata(g)
 	image.save('g%s.png' % k)
-
-	#image.putdata(b)
-	#image.save('b%s.png' % k)
-	print('g%s.png' % k)
+    print('g%s.png' % k)
+        
+	image.putdata(b)
+	image.save('b%s.png' % k)
+	print('b%s.png' % k)
+    
+    #update name counter
 	k += 1
 
 
